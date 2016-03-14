24447850f2a6a3e6bd3ef5a5f41335b652c18484
Eric Delord <edelord@gmail.com>
2016-03-15 00:03:17 +0100

    updated pin colors

--- editolido/constants.py
+++ editolido/constants.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-PIN_PINK = 0
-PIN_YELLOW = 1
-PIN_RED = 2
-PIN_GREEN = 3
-PIN_BLUE = 4
-PIN_NONE = 5
-PINS = ('#placemark-pink', '#placemark-yellow', '#placemark-red',
-        '#placemark-green', '#placemark-blue')
+
+PIN_NONE = 0
+PIN_BLUE = 1
+PIN_YELLOW = 2
+PIN_BROWN = 3
+PIN_ORANGE = 4
+PIN_PINK = 5
+PIN_RED = 6
+PIN_GREEN = 7
+PIN_PURPLE = 8
+PINS = ('#placemark-none', '#placemark-blue', '#placemark-yellow',
+        '#placemark-brown', '#placemark-orange', '#placemark-pink',
+        '#placemark-red', '#placemark-green', '#placemark-purple')
 
 NAT_POSITION_ENTRY = 0
 NAT_POSITION_EXIT = 1
