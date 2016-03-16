a712f58e828d07e13984b3564c26d414bd79a2f6
Eric Delord <edelord@gmail.com>
2016-03-16 15:10:20 +0100

    removed remaining global import

--- editolido/__init__.py
+++ editolido/__init__.py
@@ -1,7 +1,3 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-# from ofp import OFP
-# from route import Route
-# from geopoint import GeoPoint, dm_normalizer, arinc_normalizer
 
-__version__ = '1.0.0b2'
+__version__ = '1.0.0b3'
--- editolido/workflows/__init__.py
+++ editolido/workflows/__init__.py
@@ -1,3 +1,1 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-from lido2mapsme import lido2mapsme
