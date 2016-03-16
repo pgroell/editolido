4b0d81aaace9b3257827d0e73b1dcb5dd50509f3
Eric Delord <edelord@gmail.com>
2016-03-16 16:10:49 +0100

    refactoring of ogimet

---
+++ editolido/ogimet.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from editolido.geoindex import GeoGridIndex
+from editolido.geolite import km_to_rad, rad_to_km
+from editolido.route import Route
+
+
+def ogimet_route(route, segment_size=300, debug=False):
+	wmo_grid = GeoGridIndex()
+	wmo_grid.load()
+	start = route[0]
+	end = route[-1]
+
+	def print_ogimet(points):
+		print 'Route Ogimet (%s): %s' % (
+			len(points), '_'.join([p.name for p in points]))
+
+	# noinspection PyShadowingNames
+	def build_ogimet(default_step):
+		ogimet_sites = [start.name]
+		previous = start
+		ogimet_points = [start]
+		sid = True
+		for i, p in enumerate(
+		        route.split(60, converter=km_to_rad, preserve=True)):
+			if i == 0:
+				continue
+			neighbours = sorted(
+				wmo_grid.get_nearest_points(p, 30, converter=km_to_rad),
+				key=lambda t: t[1])
+			if neighbours:
+				point, d = neighbours[0]
+				if sid and point.distance_to(start, converter=rad_to_km) < 500:
+					step = min(60, default_step)
+				else:
+					sid = False
+					step = default_step
+				if point.name not in ogimet_sites and previous.distance_to(
+				        point, converter=rad_to_km) > step:
+					previous = point
+					ogimet_points.append(point)
+					ogimet_sites.append(point.name)
+		ogimet_points[-1] = end
+		return ogimet_points
+
+	step = start.distance_to(end, converter=rad_to_km) / 200
+	ogimet_points = []
+	while True:
+		ogimet_points = build_ogimet(step)
+		if len(ogimet_points) < 23:
+			break
+		if debug:
+			print_ogimet(ogimet_points)
+		step *= 2
+	if debug:
+		print_ogimet(ogimet_points)
+	return Route(ogimet_points).split(segment_size, preserve=True)
--- editolido/workflows/lido2mapsme.py
+++ editolido/workflows/lido2mapsme.py
@@ -2,15 +2,14 @@
 from __future__ import unicode_literals
 
 from editolido.constants import NAT_POSITION_ENTRY, PIN_NONE
-from editolido.geoindex import GeoGridIndex
-from editolido.geolite import km_to_rad, rad_to_km
 from editolido.geopoint import GeoPoint
 from editolido.kml import KMLGenerator
 from editolido.ofp import OFP
 from editolido.route import Route
+from editolido.ogimet import ogimet_route
 
 
-def lido2mapsme(action_in, params):
+def lido2mapsme(action_in, params, debug=False):
 	ofp = OFP(action_in)
 	kml = KMLGenerator()
 
@@ -42,51 +41,7 @@ def lido2mapsme(action_in, params):
 		               excluded=natmarks, style=params['Point Route'])
 
 	if params['Afficher Ogimet']:
-		wmo_grid = GeoGridIndex()
-		wmo_grid.load()
-		start = route[0]
-		end = route[-1]
-		#d = start.distance_to(end, converter=rad_to_km)
-
-		def build_ogimet(default_step):
-			ogimet_sites = [start.name]
-			previous = start
-			ogimet_points = [start]
-			sid = True
-			for i, p in enumerate(route.split(60, converter=km_to_rad, preserve=True)):
-				if i == 0:
-					continue
-				neighbours = sorted(
-					wmo_grid.get_nearest_points(p, 30, converter=km_to_rad),
-					key=lambda t: t[1])
-				if neighbours:
-					point, d = neighbours[0]
-					if sid and point.distance_to(start, converter=rad_to_km) < 500:
-						step = min(60, default_step)
-					else:
-						sid = False
-						step = default_step
-					if point.name not in ogimet_sites and previous.distance_to(
-					        point, converter=rad_to_km) > step:
-						previous = point
-						ogimet_points.append(point)
-						ogimet_sites.append(point.name)
-			ogimet_points[-1] = end
-			return ogimet_points
-		step = start.distance_to(end, converter=rad_to_km) / 200
-		ogimet_points = []
-		while True:
-			ogimet_points = build_ogimet(step)
-			if len(ogimet_points) < 23:
-				break
-			ogimet_sites = [p.name for p in ogimet_points]
-			print 'Route Ogimet (%s): %s' % (len(ogimet_sites), '_'.join(ogimet_sites))
-			step *= 2
-		ogimet_sites = [p.name for p in ogimet_points]
-		# clipboard.set('_'.join(ogimet_sites))
-		print 'Route Ogimet (%s): %s' % (len(ogimet_sites), '_'.join(ogimet_sites))
-		ortho_ogimet = Route(ogimet_points).split(300, preserve=True)
-		kml.add_line('ogimet', ortho_ogimet)
+		kml.add_line('ogimet', ogimet_route(route, debug=debug))
 
 	return kml.render(
 		name=ofp.description,
