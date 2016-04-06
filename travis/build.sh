1ab867a9c11646985b1f4e5fb2c276cf9427b0c3
Eric Delord <edelord@gmail.com>
2016-04-06 22:17:59 +0200

    try to ignore error

--- travis/build.sh
+++ travis/build.sh
@@ -10,7 +10,7 @@ ls -al ${PWD}/wkhtmltopdf/wkhtmltox/bin/
 cd ./tuto
 echo $PATH
 pandoc --from=markdown+yaml_metadata_block -t html5 --template=../pandoc/pandoc_tpl.html -s tuto.md -o ${TRAVIS_BUILD_DIR}/tuto.html
-wkhtmltopdf ./tuto.html tuto.pdf
+wkhtmltopdf --load-error-handling ignore tuto.html tuto.pdf
 
 echo '######################################'
 echo '#                Build               #'
